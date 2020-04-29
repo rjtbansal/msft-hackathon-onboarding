4bb380ee52a672f3470598551a43fe789af4950c
Rajat Bansal <rjtbansal@gmail.com>
2020-04-29 11:26:25 -0400

    added different landing page image and some tweaks

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ import backgroundImage from '../../assets/images/landing-page-background.png';
 
 const LandingPage = () => {
   return<div className="landing-page"> 
-          
+           <img className="myImg" src={backgroundImage} alt="" />
           <Link to='/office'><button className="demo-button button1">DEMO</button></Link>
           <Link to='/office'><button className="demo-button button2">DEMO</button></Link>
           <Link to='/office'><button className="demo-button button3">DEMO</button></Link>

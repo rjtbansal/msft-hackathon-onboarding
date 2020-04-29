7802b5c0e1a9a6362ea2a2154a17e3750364be1c
Rajat Bansal <rjtbansal@gmail.com>
2020-04-29 09:06:20 -0400

    replaced background image with an image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './LandingPage.scss';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../../assets/images/landing-page-background.png';
+
 const LandingPage = () => {
   return<div className="landing-page"> 
+           <img className="myImg" src={backgroundImage} alt="" />
           <Link to='/office'><button className="demo-button button1">DEMO</button></Link>
+          <Link to='/office'><button className="demo-button button2">DEMO</button></Link>
+          <Link to='/office'><button className="demo-button button3">DEMO</button></Link>
+          <Link to='/office'><button className="demo-button button4">DEMO</button></Link>
         </div>
 }
 
